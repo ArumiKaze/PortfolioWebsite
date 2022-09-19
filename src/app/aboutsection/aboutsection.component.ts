23619a6ef4475f3431e96688489affeeaac6e585
ArumiKaze <imura.eisaku@gmail.com>
2022-09-18 23:22:11 -0700

    wip progress on About section

--- src/app/aboutsection/aboutsection.component.ts
+++ src/app/aboutsection/aboutsection.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { GeneralService } from '../general.service';
 
 @Component({
   selector: 'app-aboutsection',
   templateUrl: './aboutsection.component.html',
   styleUrls: ['./aboutsection.component.css']
 })
-export class AboutsectionComponent implements OnInit {
+export class AboutsectionComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  unsubOnDestroy = new Subject<boolean>();
+  isDarkMode!: boolean;
+
+  constructor(private generalService: GeneralService) { }
 
   ngOnInit(): void {
+    this.isDarkMode = this.generalService.GetDarkModeStatus();
+    this.generalService.subjectToggleDarkMode.pipe(takeUntil(this.unsubOnDestroy)).subscribe({
+      next: () => {
+        this.isDarkMode = !this.isDarkMode;
+      }
+    });
   }
 
+  ngOnDestroy(): void {
+    this.unsubOnDestroy.next(true);
+    this.unsubOnDestroy.complete();
+  }
 }
