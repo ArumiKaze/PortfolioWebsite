89cdacc8554f97226326bb1ad6248e097e33816f
ArumiKaze <imura.eisaku@gmail.com>
2022-10-20 22:47:54 -0700

    change port number of express api

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(cors());
